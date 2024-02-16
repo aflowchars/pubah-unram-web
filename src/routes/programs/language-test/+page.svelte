8511ab8feb8673a7fd3468ea7f08895704e09d40
Afochar <aflowchars@gmail.com>
2024-02-16 14:29:52 +0700

    update language test

---
+++ src/routes/programs/language-test/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+	import { SeoHead } from '$lib/components/base';
+</script>
+
+<SeoHead title="Tes Kemampuan Bahasa Asing" />
+
+<div class="px-10">
+	<header class="relative h-[50vh] overflow-hidden">
+		<img
+			class="absolute inset-0 -z-10 h-full w-full object-cover object-center"
+			src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="Language Test"
+		/>
+
+		<div
+			class="absolute inset-0 z-0 h-full w-full bg-gradient-to-t from-gray-950/80 to-gray-50/25"
+		/>
+
+		<div
+			class="relative z-10 flex h-full w-full flex-col justify-end p-10 text-white"
+		>
+			<nav class="w-3/4 space-y-2.5 text-left">
+				<h1 class="text-4xl font-medium tracking-tighter">
+					Tes Kemampuan Bahasa Asing
+				</h1>
+
+				<p class="w-7/12 text-base leading-relaxed text-gray-300">
+					Tes Kemampuan Bahasa Asing adalah tes yang dilakukan oleh mahasiswa
+					Unram untuk mengetahui kemampuan bahasa Inggris mahasiswa.
+				</p>
+			</nav>
+		</div>
+	</header>
+
+	<section
+		class="mx-auto mt-10 max-w-screen-lg px-10 text-justify text-lg leading-loose text-gray-600"
+	>
+		<p>
+			Layanan bahasa asing di UPA Bahasa Universitas Mataram menyelenggarakan
+			Tes TOEFL Prediksi dan Tes TOEFL Standar untuk mengukur kemampuan
+			berbahasa Inggris mahasiswa dan masyarakat umum. Tes TOEFL Prediksi hadir
+			dalam dua format, yaitu tes berbasis komputer dan tes yang menggunakan
+			Lembar Jawaban Kertas. Selain mempersiapkan peserta untuk ujian resmi
+			TOEFL, tes ini bertujuan untuk meningkatkan keterampilan berbahasa Inggris
+			secara umum. Pelaksanaan tes dilakukan sesuai jadwal yang telah
+			ditentukan, dan biasanya memerlukan biaya pendaftaran. Hasil tes ini juga
+			dapat digunakan untuk menilai kemampuan bahasa Inggris mahasiswa selama
+			studi mereka di Universitas Mataram. Peserta yang mengikuti tes ini akan
+			mendapatkan manfaat dalam mengetahui seberapa siap mereka menghadapi ujian
+			resmi TOEFL serta meningkatkan keterampilan berbahasa Inggris mereka.
+			Instruksi dan persiapan untuk tes dapat diperoleh melalui saran tentang
+			bahan studi yang berguna dan strategi efektif dalam menghadapi tes ini.
+		</p>
+	</section>
+</div>
+
+<nav class="mt-10 flex items-center justify-between bg-blue-50 px-32 py-5">
+	<a href="/programs/course" class="flex flex-col items-start">
+		<h6 class="text-xs font-semibold text-gray-500">Sebelumnya</h6>
+
+		<p class="text-lg font-semibold text-gray-950">Course</p>
+	</a>
+
+	<a
+		href="/programs/translation-&-interpreting"
+		class="flex flex-col items-end"
+	>
+		<h6 class="text-xs font-semibold text-gray-500">Selanjutnya</h6>
+
+		<p class="text-lg font-semibold text-gray-950">
+			Translation & Interpreting
+		</p>
+	</a>
+</nav>
